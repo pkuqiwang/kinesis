af005dfbf8017ffc2cc09476a2cfefcc950a724c
Wang <qwanga@147dda2fef2c.ant.amazon.com>
2020-12-18 22:27:44 -0500

    update readme and uncomment code for publish message

--- PutRecord_Kinesis.py
+++ PutRecord_Kinesis.py
@@ -10,15 +10,16 @@ def generate(stream_name, kinesis_client):
     with open("./sample.csv", encoding='utf-8') as csvf:
         csvReader = csv.DictReader(csvf)
         for rows in csvReader:
-            #limit the number of records being sent to Kinesis
+            # limit the number of records being sent to Kinesis
             # included sample file contains 100,000 row in total
-            if (counter >= 10):
+            # it will take more than a hour to send all the messages
+            if (counter >= 1000):
                 break
             counter = counter + 1
             print(json.dumps(rows))
-            # kinesis_client.put_record(StreamName=stream_name,
-            #                           Data=json.dumps(rows),
-            #                           PartitionKey="partitionkey")
+            kinesis_client.put_record(StreamName=stream_name,
+                                      Data=json.dumps(rows),
+                                      PartitionKey="partitionkey")
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# kinesis
+# How to use Python Kinesiis agent to Push meesages into Kinesis Data stream
+ 
+this is a code sample of how to use Python to push large numebr of code into Kinesis data test in the readme
+
+Use the following code in AWS CLI to create the Kinesis data stream for the sample
+```aws kinesis create-stream --stream-name ExampleInputStream --shard-count 1```
+
+then use `PutRecond_Kinesis.py` to send the messages to the Kinesis data stream you just created.
+
+
